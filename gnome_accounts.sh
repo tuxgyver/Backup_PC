7e715fa8d3f29a146ca49a09c7fd607a16407928
tuxgyver <7334622+tuxgyver@users.noreply.github.com>
2025-08-07 07:50:00 +0200

    Update gnome_accounts.sh

--- gnome_accounts.sh
+++ gnome_accounts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script de sauvegarde et restauration des comptes et partages réseau GNOME
-# Auteur: Assistant Claude
+# Auteur: Fontaine Johnny
 # Version: 1.0
 
 # Couleurs pour l'affichage
